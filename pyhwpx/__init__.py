ecb4489acf7e0c291128f31ba0bc5b4019959635
ilco <martinii.fun@gmail.com>
2025-03-27 12:29:43 +0900

    __init__에 다시 from pyhwpx import *을 넣어보았다...

--- pyhwpx/__init__.py
+++ pyhwpx/__init__.py
@@ -1,14 +1,15 @@
-import sys
+# import sys
+#
+# if not getattr(sys, 'frozen', False):  # pyinstaller 실행 환경이 아니면
+#     from .pyhwpx import Hwp
+# else:
+#     # pyinstaller에서는 지연 import
+#     import types
+#     def _get_Hwp():
+#         from .pyhwpx import Hwp
+#         return Hwp
+#
+#     sys.modules[__name__].Hwp = _get_Hwp()
 
-if not getattr(sys, 'frozen', False):  # pyinstaller 실행 환경이 아니면
-    from .pyhwpx import Hwp
-else:
-    # pyinstaller에서는 지연 import
-    import types
-    def _get_Hwp():
-        from .pyhwpx import Hwp
-        return Hwp
-
-    sys.modules[__name__].Hwp = _get_Hwp()
-
+from .pyhwpx import *
 from .version import __version__
