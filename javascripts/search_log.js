29909c672ff05903ff81bb34de0cfc735482dd52
ilco <martinii.fun@gmail.com>
2025-03-28 13:35:49 +0900

    Deployed ea910a6 with MkDocs version: 1.6.1

--- javascripts/search_log.js
+++ javascripts/search_log.js
@@ -1,18 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const searchInput = document.querySelector('input.md-search__input');
+    const searchInput = document.querySelector('input.md-search__input');
 
-  if (searchInput) {
-    searchInput.addEventListener('keydown', function (e) {
-      if (e.key === 'Enter') {
-        const query = searchInput.value;
+    if (searchInput) {
+        searchInput.addEventListener('keydown', function (e) {
+            if (e.key === 'Enter') {
+                const query = searchInput.value;
 
-        fetch('https://script.google.com/macros/s/AKfycbz64ekHwL55CDk_zhDSHaLu-sCHfNOpue4PH8-UydxkHyZ-Ei9BtJEKzK98eWj_VZe64w/exec', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ query })
-        })
-        
-      }
-    });
-  }
+                fetch('https://script.google.com/macros/s/AKfycbzC7rgzGmi1u0-IzmpDDZin6iwocXaljPA8FpVH3vXwMmcXdDJ6DED--81lRyo5IV4-Pg/exec', {
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify({query})
+                })
+                    .then(response => response.text())
+                    .then(data => console.log("✅ 검색어 전송 성공:", data))
+                    .catch(error => console.error("❌ 검색어 전송 실패:", error));
+            }
+        });
+    }
 });
